e3a419d3195d52c531c5d302187acb2b77b49972
sfwsf <sfsfsf>
2022-06-02 16:20:49 +0300

    test

---
+++ _next/static/mwke-jjUfMAKlB8-zjEqk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-49ca0ed02346440c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/sandbox":["static/chunks/pages/sandbox-2f6d74a5ae032603.js"],sortedPages:["/","/_app","/_error","/sandbox"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
